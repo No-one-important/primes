891c2e4539a713ffcbbd1b00aab68453e5acc22d
Aaron <aaron.huntley2007@gmail.com>
2021-03-20 17:05:34 +0000

    simple check all

--- primes.js
+++ primes.js
@@ -1,1 +1,8 @@
-
+function simple_check_all(num) {
+    for (let x = 2; x < Math.ceil(Math.sqrt(num)); x++) {
+        if num % x == 0 {
+            return false
+        }
+    }
+    return true
+}
